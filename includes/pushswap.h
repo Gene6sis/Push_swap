bd26ca448d5677d5c872cbfb32c346e08039c223
Adam BEN MCHICHI <adben-mc@e2r8p12.clusters.42paris.fr>
2022-02-03 18:01:07 +0100

    update

--- includes/pushswap.h
+++ includes/pushswap.h
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   pushswap.h                                         :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: adam <adam@student.42.fr>                  +#+  +:+       +#+        */
+/*   By: adben-mc <adben-mc@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/02/02 01:04:55 by adben-mc          #+#    #+#             */
-/*   Updated: 2022/02/02 15:51:49 by adam             ###   ########.fr       */
+/*   Updated: 2022/02/03 15:35:07 by adben-mc         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,18 +18,18 @@
 # include <stdlib.h>
 
 typedef struct s_stack {
-	size_t	position;
-	int		number;
-	void	*next;
+	size_t			position;
+	int				number;
+	void			*next;
 	struct s_list	*previous;
 }				t_stack;
 
-
 typedef struct s_data {
-	char	**argv;
-	int		argc;
-	t_stack	stackA;
-	t_stack	stackB;
+	char			**argv;
+	int				argc;
+	t_stack			stacka;
+	t_stack			stackb;
+	int				nb_number;
 }				t_data;
 
 #endif
--- srcs/main.c
+++ srcs/main.c
@@ -3,26 +3,48 @@
 /*                                                        :::      ::::::::   */
 /*   main.c                                             :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: adam <adam@student.42.fr>                  +#+  +:+       +#+        */
+/*   By: adben-mc <adben-mc@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/02/02 00:52:34 by adben-mc          #+#    #+#             */
-/*   Updated: 2022/02/02 16:03:38 by adam             ###   ########.fr       */
+/*   Updated: 2022/02/03 18:00:56 by adben-mc         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../includes/pushswap.h"
 
-int	ft_validarg(t_data *data)
+int	ft_have_nb(char *str)
+{
+	size_t	i;
+	int		number;
+
+	number = 0;
+	i = 0;
+	while (str[i])
+	{
+		if (ft_strchr("0123456789", str[i]))
+			number = 1;
+		i++;
+	}
+	return (number);
+}
+
+int	ft_validarg(int argc, char **argv)
 {
 	int	i;
 	int	j;
+
+	if (argc < 2)
+	{
+		ft_printf("At least 2 arguments are needed\n");
+		return (0);
+	}
 	i = 1;
-	while (i < data->argc)
+	while (i < argc)
 	{
 		j = 0;
-		while (data->argv[i][j])
+		while (argv[i][j])
 		{
-			if (!ft_strchr("-+0123456789 ", data->argv[i][j]))
+			if (!ft_strchr("-+0123456789 ", argv[i][j]) || !ft_have_nb(argv[i]))
 			{
 				ft_printf("Invalid character\n");
 				return (0);
@@ -34,20 +56,89 @@ int	ft_validarg(t_data *data)
 	return (1);
 }
 
-void	ft_parsing(t_data *data, char **argv, int argc)
+//a.out ""  b c
+//00000 "" 	1 2		3
+char	*ft_strjoinbis(char *s1, char *s2)
+{
+	char	*str;
+	size_t	lens1;
+	size_t	lens2;
+
+	if (!s1 || !s2)
+		return (NULL);
+	lens1 = ft_strlen(s1);
+	lens2 = ft_strlen(s2);
+	str = malloc(sizeof(char) * (lens1 + lens2 + 1));
+	if (!str)
+		return (NULL);
+	ft_memcpy(str, s1, lens1);
+	ft_memcpy(str + lens1, s2, lens2);
+	str[lens1 + lens2] = '\0';
+	free(s1);
+	free(s2);
+	return (str);
+}
+
+char	**ft_preparestack(t_data	*data)
+{
+	char	*str;
+	int		i;
+	char	**split;
+
+	i = 1;
+	str = ft_strdup("");
+	while (i < data->argc)
+	{
+		str = ft_strjoinbis(ft_strjoinbis(str, ft_strdup(" ")),
+				ft_strdup(data->argv[i++]));
+		if (!str)
+			return (NULL);
+	}
+	split = ft_split(str, ' ');
+	free(str);
+	return (split);
+}
+
+int	ft_initstack(t_data	*data)
+{
+	char	**split;
+	size_t	i;
+
+	split = ft_preparestack(data);
+	if (!split)
+		return (0);
+	if (!ft_validarg(data->argc, split))
+		return (0);
+	data->nb_number = 0;
+	while (split[data->nb_number])
+		data->nb_number++;
+	return (1);
+}
+
+int	ft_parsing(t_data *data, char **argv, int argc)
 {
 	data->argv = argv;
 	data->argc = argc;
-	if (!ft_validarg(data))
-		exit(0);
+	if (!ft_validarg(data->argc, data->argv))
+		return (0);
+	if (!ft_initstack(data))
+		return (0);
+	return (1);
 }
+
+// void	ft_printstacks(t_data	*data)
+// {
+// 	size_t	i;
+
+	
+// }
 
 int	main(int argc, char **argv)
 {
 	t_data	data;
 
-	if (argc < 2)
-		return (0);
-	ft_parsing(&data, argv, argc);
-	ft_printf("coucou");
+	if (!ft_parsing(&data, argv, argc))
+		return (1);
+	ft_printf("Parsing done ;)\n");
+	ft_printstacks(&data);
 }
