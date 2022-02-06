74ba0f5479caef31bd45f428160d858c7fccfa08
Adam BEN MCHICHI <adben-mc@e2r2p7.clusters.42paris.fr>
2022-02-06 05:16:15 +0100

    move2

--- srcs/reverse_rotate.c
+++ srcs/reverse_rotate.c
@@ -6,14 +6,21 @@
 /*   By: adben-mc <adben-mc@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/02/06 00:43:27 by adben-mc          #+#    #+#             */
-/*   Updated: 2022/02/06 00:48:18 by adben-mc         ###   ########.fr       */
+/*   Updated: 2022/02/06 05:15:41 by adben-mc         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../includes/pushswap.h"
 
-void	rra(t_stack *stack);
+void	rra(t_stack *stack)
+{
+	
+}
 
 void	rrb(t_stack *stack);
 
-void	rrr(t_stack *stack);
+void	rrr(t_stack *stack)
+{
+	rra(stack);
+	rrb(stack);
+}
--- srcs/rotate.c
+++ srcs/rotate.c
@@ -6,14 +6,61 @@
 /*   By: adben-mc <adben-mc@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/02/06 00:42:46 by adben-mc          #+#    #+#             */
-/*   Updated: 2022/02/06 00:48:14 by adben-mc         ###   ########.fr       */
+/*   Updated: 2022/02/06 05:15:10 by adben-mc         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../includes/pushswap.h"
 
-void	ra(t_stack *stack);
+void	ra(t_stack *stack)
+{
+	t_stack *cur;
+	t_stack *follow;
 
-void	rb(t_stack *stack);
+	cur = stack;
+	follow = cur->next;
+	if (!cur || !follow)
+	{
+		ft_printf("ra on NULL\n");
+		return ;
+	}
+	while (follow)
+	{
+		cur->number = follow->number;
+		cur = cur->next;
+		follow = follow->next;
+	}
+	follow->next = stack;
+	stack->next = NULL;
+}
 
-void	rr(t_stack *stack);
+void	rb(t_stack *stack)
+{
+	t_stack *cur;
+	t_stack *follow;
+	t_stack *first;
+
+	cur = stack;
+	follow = cur->next;
+	first = follow;
+	if (!cur || !follow)
+	{
+		ft_printf("ra on NULL\n");
+		return ;
+	}
+	while (follow)
+	{
+		cur->number = follow->number;
+		cur = cur->next;
+		follow = follow->next;
+	}
+	follow->next = stack;
+	stack->next = NULL;
+	stack = first;
+}
+
+void	rr(t_stack *stack)
+{
+	ra(stack);
+	rb(stack);
+}
